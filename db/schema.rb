45912548225819d18375b01b64059a25b5462920
Justin Campbell <justin@justincampbell.me>
2018-04-19 19:07:30 -0400

    Add admin bool to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419174407) do
+ActiveRecord::Schema.define(version: 20180419214639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20180419174407) do
     t.string "sms_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
   create_table "verification_codes", force: :cascade do |t|
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
     sms_number { Faker::PhoneNumber.cell_phone }
+
+    trait :admin do
+      admin true
+    end
   end
 end
