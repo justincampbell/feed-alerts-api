fa9ea3610b6c7a0c005cced80087acc06f20dfc3
Justin Campbell <justin@justincampbell.me>
2018-05-04 22:51:39 -0400

    Clean up migration

--- db/migrate/20180505023515_change_events_to_polymorphic_resource.rb
+++ db/migrate/20180505023515_change_events_to_polymorphic_resource.rb
@@ -1,11 +1,10 @@
 class ChangeEventsToPolymorphicResource < ActiveRecord::Migration[5.1]
   def change
     remove_index :events, :user_id
-    add_column :events, :resource_type, :string
-    Event.update_all resource_type: "users"
-    change_column :events, :resource_type, :string, null: false
+    remove_foreign_key :events, :users
+
+    add_column :events, :resource_type, :string, null: false
     rename_column :events, :user_id, :resource_id
     add_index :events, [:resource_type, :resource_id]
-    remove_foreign_key :events, :users
   end
 end
